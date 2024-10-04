71cbf1f2e07174854806ab6d609afb060981d290
Пользователь <dailos91@mail.ru>
2024-10-04 11:08:06 +0300

    Fix

--- kittens/kittensapi/urls.py
+++ kittens/kittensapi/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 from .views import (
     KittenViewSet,
@@ -8,6 +13,7 @@ from .views import (
 
 app_name = 'kittensapi'
 
+
 routers = DefaultRouter()
 
 routers.register("kittens", KittenViewSet)
@@ -15,4 +21,7 @@ routers.register("breed", BreedViewSet)
 
 urlpatterns = [
     path('', include(routers.urls)),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
